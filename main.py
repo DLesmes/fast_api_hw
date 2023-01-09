4923a7fa516ef2077830d56523cbd6e2a9742902
DLesmes <ing.dlesmes@gmail.com>
2023-01-09 14:58:13 -0500

    path operators decorator and function

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def home():
+@app.get("/") # Called Path operation decorator
+def home(): # Called path operation function
     return {"message": "Hello World"}
