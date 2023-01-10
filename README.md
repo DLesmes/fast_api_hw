5a6912c8dfed2298da501e442a651f3d6ff65f12
DLesmes <ing.dlesmes@gmail.com>
2023-01-09 23:42:05 -0500

    validations query

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 * GitHub - Redocly/redoc: 📘 [OpenAPI/Swagger-generated API Reference Documentation](https://github.com/Redocly/redoc)
 * [API Documentation & Design Tools for Teams | Swagger](https://swagger.io/)
 * [Clase repo](https://github.com/platzi/curso-fastapi-fundamentos-path-validaciones/blob/request_response_body/main.py)
+* [Query params str validations](https://fastapi.tiangolo.com/tutorial/query-params-str-validations/)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 app = FastAPI()
 
@@ -29,3 +29,12 @@ def home(): # Called path operation function
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): 
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+): 
+    return {name: age}
