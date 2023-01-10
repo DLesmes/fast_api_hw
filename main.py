9e07da2f87b7ac5ec993f09243591d2dfafe6334
DLesmes <ing.dlesmes@gmail.com>
2023-01-09 23:25:27 -0500

    Request and Response Body finished class

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+# Models
+
+class Person(BaseModel): 
+    first_name: str
+    last_name: str
+    age: int 
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 @app.get("/") # Called Path operation decorator
